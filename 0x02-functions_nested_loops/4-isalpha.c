1479125d96d993e962841828487745db74aab3fb
Skhetha22 <rabsonncubez@gmail.com>
2023-09-13 08:30:03 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,25 +1,13 @@
-
 #include "main.h"
-
 /**
-
- * _isalpha - Checks for alphabetic order
-
- *
-
- * @c: alpabet to check for
-
- *
-
- * Return: 1 if c is a letter, 0 otherwise
-
- */
-
+* _isalpha - Checks for alphabetic order
+*
+* @c: alpabet to check for
+*
+* Return: 1 if c is a letter, 0 otherwise
+*/
 int _isalpha(int c)
-
 {
-
-        return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
-
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 
 }
